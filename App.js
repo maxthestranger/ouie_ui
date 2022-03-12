49567dfcfcd715457582c7c34868bfdb425bcd75
maxthestranger <thestrangermax@gmail.com>
2022-03-12 10:09:11 +0300

    expo project setup

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: '#fff' }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#22252C',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#fff',
+  },
+});
